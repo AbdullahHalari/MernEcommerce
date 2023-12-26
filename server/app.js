69b6ef07be1ba40578c3418390f74563134bda71
AbdullahHalari <droidabdullah@gmail.com>
2023-12-26 21:00:14 +0500

    db connect & setup

---
+++ server/app.js
@@ -0,0 +1,15 @@
+const dotenv = require("dotenv");
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+dotenv.config({ path: "./config.env" });
+require("./dbcon");
+app.use(express.json());
+app.use(cookieParser());
+// app.use(require("./auth/auth"));
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`server is running at port no ${PORT}`);
+});
